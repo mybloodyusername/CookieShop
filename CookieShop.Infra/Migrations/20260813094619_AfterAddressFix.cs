87476b4a78e13eb47a095dbadcfd7121bca2ef34
mybloodyusername <y.sanaei94@gmail.com>
2026-08-13 14:10:53 +0330

    addressFix migratin

---
+++ CookieShop.Infra/Migrations/20260813094619_AfterAddressFix.cs
@@ -0,0 +1,232 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CookieShop.Infra.Migrations
+{
+    /// <inheritdoc />
+    public partial class AfterAddressFix : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Address_AspNetUsers_UserId",
+                table: "Address");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Address_City_CityId",
+                table: "Address");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Address_Province_ProvinceId",
+                table: "Address");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_City_Province_ProvinceId",
+                table: "City");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Province",
+                table: "Province");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_City",
+                table: "City");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Address",
+                table: "Address");
+
+            migrationBuilder.RenameTable(
+                name: "Province",
+                newName: "Provinces");
+
+            migrationBuilder.RenameTable(
+                name: "City",
+                newName: "Cities");
+
+            migrationBuilder.RenameTable(
+                name: "Address",
+                newName: "Addresses");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_City_ProvinceId",
+                table: "Cities",
+                newName: "IX_Cities_ProvinceId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Address_UserId",
+                table: "Addresses",
+                newName: "IX_Addresses_UserId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Address_ProvinceId",
+                table: "Addresses",
+                newName: "IX_Addresses_ProvinceId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Address_CityId",
+                table: "Addresses",
+                newName: "IX_Addresses_CityId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Provinces",
+                table: "Provinces",
+                column: "Id");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Cities",
+                table: "Cities",
+                column: "Id");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Addresses",
+                table: "Addresses",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Addresses_AspNetUsers_UserId",
+                table: "Addresses",
+                column: "UserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Addresses_Cities_CityId",
+                table: "Addresses",
+                column: "CityId",
+                principalTable: "Cities",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Addresses_Provinces_ProvinceId",
+                table: "Addresses",
+                column: "ProvinceId",
+                principalTable: "Provinces",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Cities_Provinces_ProvinceId",
+                table: "Cities",
+                column: "ProvinceId",
+                principalTable: "Provinces",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Addresses_AspNetUsers_UserId",
+                table: "Addresses");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Addresses_Cities_CityId",
+                table: "Addresses");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Addresses_Provinces_ProvinceId",
+                table: "Addresses");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Cities_Provinces_ProvinceId",
+                table: "Cities");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Provinces",
+                table: "Provinces");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Cities",
+                table: "Cities");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Addresses",
+                table: "Addresses");
+
+            migrationBuilder.RenameTable(
+                name: "Provinces",
+                newName: "Province");
+
+            migrationBuilder.RenameTable(
+                name: "Cities",
+                newName: "City");
+
+            migrationBuilder.RenameTable(
+                name: "Addresses",
+                newName: "Address");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Cities_ProvinceId",
+                table: "City",
+                newName: "IX_City_ProvinceId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Addresses_UserId",
+                table: "Address",
+                newName: "IX_Address_UserId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Addresses_ProvinceId",
+                table: "Address",
+                newName: "IX_Address_ProvinceId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Addresses_CityId",
+                table: "Address",
+                newName: "IX_Address_CityId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Province",
+                table: "Province",
+                column: "Id");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_City",
+                table: "City",
+                column: "Id");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Address",
+                table: "Address",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Address_AspNetUsers_UserId",
+                table: "Address",
+                column: "UserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Address_City_CityId",
+                table: "Address",
+                column: "CityId",
+                principalTable: "City",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Address_Province_ProvinceId",
+                table: "Address",
+                column: "ProvinceId",
+                principalTable: "Province",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_City_Province_ProvinceId",
+                table: "City",
+                column: "ProvinceId",
+                principalTable: "Province",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
